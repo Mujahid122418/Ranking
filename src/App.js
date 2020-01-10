1c7053a6ad25597c97c02e3a5cb5e480821841f8
Mujahid <mujahid122418@gmail.com>
2020-01-10 22:50:24 +0500

    updated some style

--- src/App.js
+++ src/App.js
@@ -1,43 +1,38 @@
-import React from 'react';
-import Home from './components/home/home';
+import React from "react";
+import Home from "./components/home/home";
 import About from "./components/about/about";
 // import Footer from "./components/footer/footer.js"
-import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom"
-import Rankings from './components/ranking/rankings';
-import CertifiedProducts from './components/certifiedProducts/certifiedProducts';
-import Login from './components/auth/login';
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
+import Rankings from "./components/ranking/rankings";
+import CertifiedProducts from "./components/certifiedProducts/certifiedProducts";
+import Login from "./components/auth/login";
 
-import SignUp from './components/auth/signUp';
-import { connect } from "react-redux"
-import ViewProduct from "./components/ranking/viewProduct/viewProduct"
+import SignUp from "./components/auth/signUp";
+import { connect } from "react-redux";
+import ViewProduct from "./components/ranking/viewProduct/viewProduct";
 // Login
 function App({ user }) {
   return (
     <BrowserRouter>
       <div>
         <Switch>
-          <Route path="/about" component={About} />
+          <Route path="/about" component={About} />{" "}
           <Route path="/ranking" component={Rankings} />
           <Route path="/certified" component={CertifiedProducts} />
           <Route path="/productranking/:id" component={ViewProduct} />
-          <Route path="/login"
+          <Route
+            path="/login"
             render={() => {
-              return (
-                user ? <Redirect to="/" /> :
-                  <Login />
-              );
-            }
-            } />
-          <Route path="/signup" render={() => {
-            return (
-              user ? <Redirect to="/" /> :
-                <SignUp />
-            );
-          }
-          } />
+              return user ? <Redirect to="/" /> : <Login />;
+            }}
+          />
+          <Route
+            path="/signup"
+            render={() => {
+              return user ? <Redirect to="/" /> : <SignUp />;
+            }}
+          />
           <Route exact path="/" component={Home} />
-
-
         </Switch>
         {/* <Footer /> */}
       </div>
@@ -46,6 +41,6 @@ function App({ user }) {
 }
 const mapStateToProps = ({ Login: { user } }) => ({
   user
-})
+});
 
 export default connect(mapStateToProps, null)(App);
